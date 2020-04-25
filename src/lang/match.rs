ba9de01b05a6587c143c4fb5f782ede44acd5c73
iamazy <1448588084@qq.com>
2020-04-25 11:47:06 +0800

    Update match.rs

--- src/lang/match.rs
+++ src/lang/match.rs
@@ -1,11 +1,10 @@
-
 fn main() {
     let number = 84;
     match number {
-         0 => println!("Origin"),
-         1..=3 => println!("All"),
-         5 | 7 | 13 => println!("Bad Luck"),
-         n @ 42 => println!("hh {}",n),
-          _ => println!("Common"),
+        0 => println!("Origin"),
+        1..=3 => println!("All"),
+        5 | 7 | 13 => println!("Bad Luck"),
+        n @ 42 => println!("hh {}", n),
+        _ => println!("Common"),
     }
 }
