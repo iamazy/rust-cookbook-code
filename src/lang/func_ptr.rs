e0fcfde3272a62ad8d06f4692a7162e9653733bd
iamazy <1448588084@qq.com>
2020-04-25 11:47:03 +0800

    Update func_ptr.rs

--- src/lang/func_ptr.rs
+++ src/lang/func_ptr.rs
@@ -1,19 +1,18 @@
-
 fn main() {
-    let a =2;
-    let b =3;
-    println!("{:}",math(sum,a,b));
-    println!("{:}",math(product,a,b));
+    let a = 2;
+    let b = 3;
+    println!("{:}", math(sum, a, b));
+    println!("{:}", math(product, a, b));
 }
 
-fn math(op: fn(i32,i32)->i32,a:i32,b:i32) -> i32{
-    op(a,b)
+fn math(op: fn(i32, i32) -> i32, a: i32, b: i32) -> i32 {
+    op(a, b)
 }
 
-fn sum(a:i32,b:i32) -> i32{
-    a+b
+fn sum(a: i32, b: i32) -> i32 {
+    a + b
 }
 
-fn product(a:i32,b:i32) -> i32{
-    a*b
+fn product(a: i32, b: i32) -> i32 {
+    a * b
 }
