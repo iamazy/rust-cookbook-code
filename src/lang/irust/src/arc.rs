c754db32384df38e837ec8a947d6bbd389af7934
iamazy <1448588084@qq.com>
2020-05-17 23:45:45 +0800

    Update arc.rs

--- src/lang/irust/src/arc.rs
+++ src/lang/irust/src/arc.rs
@@ -29,9 +29,9 @@ fn main() {
     let val = Arc::new(AtomicUsize::new(5));
     for _ in 0..10 {
         let val = Arc::clone(&val);
-        thread::spawn(move||{
+        thread::spawn(move || {
             let v = val.fetch_add(1, Ordering::SeqCst);
-            println!("{:?}",v);
+            println!("{:?}", v);
         });
     }
 }
