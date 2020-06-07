1d7a2c5e20d0863b5131c7232e79d7ed7ec14de7
iamazy <1448588084@qq.com>
2020-06-07 12:48:57 +0800

    Update main.rs

--- src/io/mio/mob/src/main.rs
+++ src/io/mio/mob/src/main.rs
@@ -1,3 +1,7 @@
+extern crate byteorder;
+extern crate mio;
+extern crate slab;
+
 #[macro_use]
 extern crate log;
 extern crate env_logger;
@@ -6,6 +10,7 @@ mod server;
 mod client;
 mod connection;
 
+use env_logger::Env;
 use std::net::SocketAddr;
 use mio::Poll;
 use mio::net::TcpListener;
@@ -14,11 +19,14 @@ use crate::server::*;
 
 
 fn main() {
-    env_logger::init();
+    let env = Env::default()
+        .filter_or("MY_LOG_LEVEL", "trace")
+        .write_style_or("MY_LOG_STYLE", "always");
+
+    env_logger::init_from_env(env);
     let addr = "127.0.0.1:8000".parse::<SocketAddr>()
         .expect("Failed to parse host:port string");
     let socket = TcpListener::bind(&addr).expect("Failed to bind address");
-
     let mut poll = Poll::new().expect("Failed to create Poll");
     let mut server = Server::new(socket);
     server.run(&mut poll).expect("Failed to run server");
